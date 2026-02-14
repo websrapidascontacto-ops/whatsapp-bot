2f1ae9dd6c06fe40171b6b65294e0984045c4dde
David Flores <websrapidas.contacto@gmail.com>
2026-02-14 14:17:47 -0500

    remove models folder

--- models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const UserSchema = new mongoose.Schema({
-  email: { type: String, unique: true },
-  password: String,
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model("User", UserSchema);
